a529c13914149bcc3a0bdacc0db2701de449e2a1
yuvarajengineer <yuvarajengineer@gmail.com>
2024-03-31 14:28:19 +0530

    my_vpc

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_subnet0" {
 }
 
 resource "aws_subnet" "public_1" {
-  vpc_id     = aws_vpc.demo.id
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
@@ -31,7 +31,7 @@ resource "aws_subnet" "public_1" {
   }
 }
 resource "aws_subnet" "public_2" {
-  vpc_id     = aws_vpc.demo.id
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
@@ -41,7 +41,7 @@ resource "aws_subnet" "public_2" {
   }
 }
 resource "aws_subnet" "public_3" {
-  vpc_id     = aws_vpc.demo.id
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1c"
   map_public_ip_on_launch = true
@@ -113,15 +113,15 @@ resource "aws_subnet" "private_3" {
 }
 
 resource "aws_eip" "nat_gw_eip_1" {
-  vpc = true
+  domain = "vpc"
 }
 
 resource "aws_eip" "nat_gw_eip_2" {
-  vpc = true
+  domain = "vpc"
 }
 
 resource "aws_eip" "nat_gw_eip_3" {
-  vpc = true
+  domain = "vpc"
 }
 
 resource "aws_nat_gateway" "gw_1" {
