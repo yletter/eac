3785d9d64c9d8dcfcdb24578f74883cf18b8080b
yuvarajengineer <yuvarajengineer@gmail.com>
2024-03-31 13:12:12 +0530

    ES

--- main.tf
+++ main.tf
@@ -14,14 +14,45 @@ resource "aws_internet_gateway" "my_igw" {
 }
 
 # Create Subnet
-resource "aws_subnet" "my_subnet" {
+resource "aws_subnet" "public_subnet0" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-1a"
 }
 
+resource "aws_subnet" "public_1" {
+  vpc_id     = aws_vpc.demo.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-us-east-1a"
+  }
+}
+resource "aws_subnet" "public_2" {
+  vpc_id     = aws_vpc.demo.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-us-east-1b"
+  }
+}
+resource "aws_subnet" "public_3" {
+  vpc_id     = aws_vpc.demo.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-us-east-1c"
+  }
+}
+
 # Create Route Table
-resource "aws_route_table" "my_route_table" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.my_vpc.id
 
   route {
@@ -32,8 +63,207 @@ resource "aws_route_table" "my_route_table" {
 
 # Associate Subnet with Route Table
 resource "aws_route_table_association" "subnet_assoc" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
+  subnet_id      = aws_subnet.public_subnet0.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_1" {
+    subnet_id = aws_subnet.public_1.id
+    route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_2" {
+    subnet_id = aws_subnet.public_2.id
+    route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_3" {
+    subnet_id = aws_subnet.public_3.id
+    route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-subnet-us-east-1a"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private-subnet-us-east-1b"
+  }
+}
+
+resource "aws_subnet" "private_3" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.6.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "private-subnet-us-east-1c"
+  }
+}
+
+resource "aws_eip" "nat_gw_eip_1" {
+  vpc = true
+}
+
+resource "aws_eip" "nat_gw_eip_2" {
+  vpc = true
+}
+
+resource "aws_eip" "nat_gw_eip_3" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "gw_1" {
+  allocation_id = aws_eip.nat_gw_eip_1.id
+  subnet_id     = aws_subnet.public_1.id
+}
+
+resource "aws_nat_gateway" "gw_2" {
+  allocation_id = aws_eip.nat_gw_eip_2.id
+  subnet_id     = aws_subnet.public_2.id
+}
+
+resource "aws_nat_gateway" "gw_3" {
+  allocation_id = aws_eip.nat_gw_eip_3.id
+  subnet_id     = aws_subnet.public_3.id
+}
+
+resource "aws_route_table" "private_1" {
+    vpc_id = aws_vpc.my_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.gw_1.id
+    }
+
+    tags = {
+        Name = "private-route-table-1"
+    }
+}
+
+resource "aws_route_table" "private_2" {
+    vpc_id = aws_vpc.my_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.gw_2.id
+    }
+
+    tags = {
+        Name = "private-route-table-2"
+    }
+}
+
+resource "aws_route_table" "private_3" {
+    vpc_id = aws_vpc.my_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.gw_3.id
+    }
+
+    tags = {
+        Name = "private-route-table-3"
+    }
+}
+
+resource "aws_route_table_association" "private_1" {
+    subnet_id = aws_subnet.private_1.id
+    route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_route_table_association" "private_2" {
+    subnet_id = aws_subnet.private_2.id
+    route_table_id = aws_route_table.private_2.id
+}
+
+resource "aws_route_table_association" "private_3" {
+    subnet_id = aws_subnet.private_3.id
+    route_table_id = aws_route_table.private_3.id
+}
+
+resource "aws_security_group" "es" {
+  name = "es-sg"
+  description = "Allow inbound traffic to ElasticSearch from VPC CIDR"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = [
+          aws_vpc.my_vpc.cidr_block
+      ]
+  }
+}
+
+resource "aws_iam_service_linked_role" "es_service_linked_role" {
+  aws_service_name = "es.amazonaws.com"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_elasticsearch_domain" "es" {
+  domain_name = "yuvaraj-es-domain"
+  elasticsearch_version = "7.10"
+
+  cluster_config {
+      instance_count = 3
+      instance_type = "r6g.large.elasticsearch"
+      zone_awareness_enabled = true
+      zone_awareness_config {
+        availability_zone_count = 3
+      }
+  }
+
+  vpc_options {
+      subnet_ids = [
+        aws_subnet.private_1.id,
+        aws_subnet.private_2.id,
+        aws_subnet.private_3.id
+      ]
+      security_group_ids = [
+          aws_security_group.es.id
+      ]
+  }
+
+  ebs_options {
+      ebs_enabled = true
+      volume_size = 10
+  }
+
+  access_policies = <<CONFIG
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Action": "es:*",
+          "Principal": "*",
+          "Effect": "Allow",
+          "Resource": "arn:aws:es:us-east-1:${data.aws_caller_identity.current.account_id}:domain/yuvaraj-es-domain/*"
+      }
+  ]
+}
+  CONFIG
+
+  snapshot_options {
+      automated_snapshot_start_hour = 23
+  }
+
+  tags = {
+      Domain = "yuvaraj-es-domain"
+  }
 }
 
 # Create Security Group
@@ -68,7 +298,7 @@ resource "aws_security_group" "my_security_group" {
 resource "aws_instance" "my_ec2_instance" {
   ami                    = "ami-0c101f26f147fa7fd"
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.my_subnet.id
+  subnet_id              = aws_subnet.public_subnet0.id
   security_groups        = [aws_security_group.my_security_group.id]
   key_name               = "eac"
   associate_public_ip_address = true
@@ -133,6 +363,7 @@ resource "aws_iam_instance_profile" "ec2_s3_profile" {
   name = "ec2_s3_profile"
   role = aws_iam_role.ec2_s3_cc_role.name
 }
+
 resource "aws_iam_role_policy_attachment" "ec2_s3_cc_role_policy_attachment" {
   role       = aws_iam_role.ec2_s3_cc_role.name
   policy_arn = aws_iam_policy.s3_cc_full_access_policy.arn
