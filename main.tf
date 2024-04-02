b3b5f6da34f3320f3a3311e3a9c3da2866688260
yuvarajengineer <yuvarajengineer@gmail.com>
2024-04-02 18:17:07 +0530

    RSC

--- main.tf
+++ main.tf
@@ -223,7 +223,7 @@ resource "aws_iam_service_linked_role" "es_service_linked_role" {
 
 data "aws_caller_identity" "current" {}
 
-resource "aws_opensearch_service_domain" "es" {
+resource "aws_opensearch_domain" "es" {
   count = var.create_resource ? 1 : 0
   domain_name = "yuvaraj-es-domain"
   elasticsearch_version = "2.10"
