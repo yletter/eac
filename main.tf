f79f2beb67b4620c18cb291781c7589022dd2b1f
yuvarajengineer <yuvarajengineer@gmail.com>
2024-04-09 10:19:32 +0530

    Password

--- main.tf
+++ main.tf
@@ -256,7 +256,7 @@ resource "aws_opensearch_domain" "es" {
     #anonymous_auth_enabled         = true
     master_user_options {
       master_user_name     = "master"
-      master_user_password = "masteryuvaraj"
+      master_user_password = "Master@yuvaraj"
     }
   }
 
