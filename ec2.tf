35f3b500214691e9905874894d83fc6b7f5e8347
yuvarajengineer <yuvarajengineer@gmail.com>
2024-11-10 22:50:49 +0530

    ssm

--- ec2.tf
+++ ec2.tf
@@ -21,7 +21,19 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 2222
+    to_port     = 2222
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   
 
+  }
+  ingress {
+    from_port   = 5986
+    to_port     = 5986
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
