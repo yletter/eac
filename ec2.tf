77c0b64cf287f67ab8e300433f5a83880ff35a02
yuvarajengineer <yuvarajengineer@gmail.com>
2024-11-10 23:02:57 +0530

    ssm 3

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "master-node" {
     Name = "master"
   }
 
+  iam_instance_profile = aws_iam_instance_profile.ec2_ssm_instance_profile.name
+
   key_name = "keypairNov2024" # Replace with your key pair name
 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
@@ -43,3 +45,26 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = [
+  {
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+      Service = "ec2.amazonaws.com"
+    }
+  },
+  ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm_policy" {
+  role = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
