d33521e41a39be6b2d9b6dbb5b713af33e972b18
yuvarajengineer <yuvarajengineer@gmail.com>
2024-11-10 22:01:02 +0530

    ec2

---
+++ .github/workflows/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "master-node" {
+  ami           = "ami-005fc0f236362e99f" # Replace with your desired AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "master"
+  }
+
+  key_name = "keypairNov2024" # Replace with your key pair name
+
+  security_groups = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
