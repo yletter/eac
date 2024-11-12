4d26f1548cf3f7c3f4667986400026a4ef311d44
yuvarajengineer <yuvarajengineer@gmail.com>
2024-11-12 07:12:17 +0530

    ud

--- ec2.tf
+++ ec2.tf
@@ -7,6 +7,33 @@ resource "aws_instance" "master-node" {
   iam_instance_profile = aws_iam_instance_profile.ec2_ssm_instance_profile.name
   key_name = "keypairNov2024" # Replace with your key pair name
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  user_data = <<EOF123
+#!/bin/bash
+echo Hello World Yuvaraj 
+echo Hello World Yuvaraj > /tmp/test.txt
+pwd >> /tmp/test.txt
+
+swapoff -a
+sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
+
+lsmod | grep br_netfilter 
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt-get update  
+sudo apt-get install -y  apt-transport-https ca-certificates curl software-properties-common gnupg2
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+EOF123
+
 }
 
 resource "aws_instance" "worker1" {
